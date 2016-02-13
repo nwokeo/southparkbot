c5fd5cb5568fc203ca92d37ab8b2990f7626c428
obiike <obiike.nwoke@gmail.com>
2016-02-13 03:55:09 +0000

    add reqs, markov

---
+++ markov.py
@@ -0,0 +1,23 @@
+from smarkov import Markov
+from glob import glob
+import random
+import markovify # https://github.com/jsvine/markovify
+
+allwords = []
+
+files = glob('/home/ubuntu/workspace/markov/SouthParkData/Season*.csv')
+with open(files[random.randrange(len(files))]) as infile:
+    for line in infile:
+        words = line[line.find('"'):].replace('\n', '').replace('"', '').split(' ')
+        if words != ['']:
+            allwords.append(words)
+
+with open(files[random.randrange(len(files))]) as infile:
+    text = infile.read()
+
+chain = Markov(allwords)
+print(" ".join(chain.generate_text()))
+
+text_model = markovify.Text(text)
+for i in range(5):
+    print(text_model.make_sentence())
